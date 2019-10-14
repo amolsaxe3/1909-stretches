3518435b70abe237e3432b1bc649980b8ecc046e
amolsaxe3 <amol.saxena@gmail.com>
2019-10-14 18:52:18 -0400

    added

--- questions/07_anagrams/anagrams.js
+++ questions/07_anagrams/anagrams.js
@@ -3,6 +3,24 @@
 
 const checkAnagrams = (str1, str2) => {
   //YOUR CODE
+  let isAnagram  = false;
+  if (str1 === str2) {
+    isAnagram = true;
+  }
+
+  str1 = str1.split('');
+  str1 = str1.sort();
+  str1 = str1.join('');
+
+  str2 = str2.split('');
+  str2 = str2.sort();
+  str2 = str2.join('');
+
+  if (str1 === str2) {
+    isAnagram = true;
+  }
+
+  return isAnagram;
 };
 
 module.exports = { checkAnagrams };
