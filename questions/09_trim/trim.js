b1fd996b697ae427f4df80644dc89505060ebf3e
amolsaxe3 <amol.saxena@gmail.com>
2019-10-21 18:56:06 -0400

    added

--- questions/09_trim/trim.js
+++ questions/09_trim/trim.js
@@ -1,3 +1,17 @@
-const trim = obj => {};
+const trim = obj => {
+    let retObj = {};
+    for (let key in obj) {
+
+        if (obj[key] !== null && !obj[key] !== undefined) {
+            console.log("obj.key===>",obj.key)
+            console.log("retObj--->",retObj)
+            retObj[key] = obj[key];
+            console.log("retObj-after-->",retObj)
+        }
+       
+    }
+    console.log(retObj)
+    return retObj;
+};
 
 module.exports = { trim };
