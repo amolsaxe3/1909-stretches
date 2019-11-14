53cd223ca23a8e8f5fb0af0764f54eb2fd7a306c
amolsaxe3 <amol.saxena@gmail.com>
2019-11-13 20:34:46 -0500

    added

--- questions/13_multiply/multiply.js
+++ questions/13_multiply/multiply.js
@@ -1,7 +1,7 @@
 const multiply = (num1, num2) => {
 
     if(typeof num1 !== 'number' || typeof num2 !== 'number'){
-        throw 'error error...!!!'
+        throw new Error 'error error...!!!'
     }
 
     return num1*num2;
--- questions/15_cache/cache.js
+++ questions/15_cache/cache.js
@@ -1,11 +1,23 @@
 
+// different between arg passed in calling function with ...arg is collecting everything
+// if in the function call below if ...arg is done its destructuring...
+
 function cache(cb) {
 
     if (typeof cb !== "function"){
         throw new Error("Input must be a function.");
     } else {
+
+        const memo = {};
+
         return  function cashedAdder(arg) {
-           return cb(arg);
+            if(arg in memo){
+                return memo[arg];
+            } else {
+                memo[arg] = cb(arg);
+                return memo[arg];
+            }
+
         };
     }
 
